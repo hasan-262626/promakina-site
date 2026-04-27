26fdd6ca62c653d2cddae6c937c7100ee75baaf5
ARGE 1 <hisik@HILTAR.CORP>
2026-04-27 14:01:56 +0300

    GTM eklendi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { CookiePreferences } from "./components/cookie-preferences";
 import { FloatingWhatsApp, SiteFooterCta } from "./components/site-footer-cta";
 import { SiteHeader } from "./components/site-header";
@@ -20,7 +21,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="tr">
+      <head>
+        <Script id="gtm-script" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NJT8ZQPC');`}
+        </Script>
+      </head>
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NJT8ZQPC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <TextNormalizer />
         <SiteHeader />
         {children}
